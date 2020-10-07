c00f50e2f94fd1c1b1689b85a0080e5b7072efd7
Olle Alm <olle.alm@me.com>
2020-10-07 22:55:18 +0200

    svg

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
-console.log(console.log("%c olle.tech 🧑🏻‍💻 ", "font-size: 24px; font-weight: bold; color: white; background-color: black; padding: 40px; margin: 10px;")
-)
+console.log(
+  "%c🧑🏻‍💻 olle.tech ",
+  "font-size: 24px; font-weight: bold; color: white; background-color: black; padding: 40px; margin: 10px;"
+);
